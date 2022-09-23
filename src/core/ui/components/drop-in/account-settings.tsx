4b979300e5f26bef3e77ed7705be8957cfee3c90
steve <steve21johnson@gmail.com>
2022-09-23 11:41:36 -0700

    Header, SubHeader

--- src/core/ui/components/drop-in/account-settings.tsx
+++ src/core/ui/components/drop-in/account-settings.tsx
@@ -1,7 +1,17 @@
 export const AccountSettings = () => {
   return (
     <div>
-      <h1>Account Settings</h1>
+      <Header>Account Settings</Header>
+      <SubHeader>
+        Your account settings and connected social accounts can be ajdusted
+        here.
+      </SubHeader>
     </div>
   );
 };
+const Header = ({ children }) => (
+  <div style={{ fontWeight: 600, fontSize: 28, lineHeight: '34px' }}>
+    {children}
+  </div>
+);
+const SubHeader = ({ children }) => <div>{children}</div>;
