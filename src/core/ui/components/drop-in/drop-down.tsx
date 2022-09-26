d512793267c6cb92a2a2a03eeeb3d28ab273a9bb
steve <steve21johnson@gmail.com>
2022-09-26 09:05:27 -0700

    DropDown

--- src/core/ui/components/drop-in/drop-down.tsx
+++ src/core/ui/components/drop-in/drop-down.tsx
@@ -1,3 +1,19 @@
+import { useUser } from '../../context/user';
+import { useState } from 'react';
+
 export const DropDown = () => {
-  return <div></div>;
+  const user = useUser();
+  const [isOpen, setIsOpen] = useState(true);
+  return (
+    <div>
+      <div>
+        <img src={''} />
+        {user.account?.wallet.default}
+      </div>
+      {isOpen && <div></div>}
+    </div>
+  );
 };
+const Section = ({ children }) => (
+  <div style={{ fontSize: 16, lineHeight: '20px' }}>{children}</div>
+);
