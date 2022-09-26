fb54a2fa0d9ae1614de0a9d0cc034edbdd15c14b
steve <steve21johnson@gmail.com>
2022-09-26 11:54:13 -0700

    logoutIcon

--- src/core/ui/components/drop-in/drop-down.tsx
+++ src/core/ui/components/drop-in/drop-down.tsx
@@ -10,7 +10,15 @@ export const DropDown = () => {
         {profilePicturePlaceholder}
         {user.account?.wallet.default}
       </div>
-      {isOpen && <Content>content</Content>}
+      {isOpen && (
+        <Content>
+          <Section>Manage account</Section>
+          <Section>
+            {logoutIcon}
+            Sign out
+          </Section>
+        </Content>
+      )}
     </DropDownDiv>
   );
 };
@@ -38,7 +46,7 @@ const Content = ({ children }) => (
   </div>
 );
 const Section = ({ children }) => (
-  <div style={{ fontSize: 16, lineHeight: '20px' }}>{children}</div>
+  <div style={{ fontSize: 12, lineHeight: '20px' }}>{children}</div>
 );
 const profilePicturePlaceholder = (
   <svg
@@ -61,6 +69,23 @@ const profilePicturePlaceholder = (
       cy="16"
       r="4"
       stroke="white"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
+const logoutIcon = (
+  <svg
+    width="18"
+    height="16"
+    viewBox="0 0 18 16"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M13.1667 11.3335L16.5 8.00016M16.5 8.00016L13.1667 4.66683M16.5 8.00016L4.83333 8.00016M9.83334 11.3335V12.1668C9.83334 13.5475 8.71405 14.6668 7.33334 14.6668H4C2.61929 14.6668 1.5 13.5475 1.5 12.1668V3.8335C1.5 2.45278 2.61929 1.3335 4 1.3335H7.33334C8.71405 1.3335 9.83334 2.45278 9.83334 3.8335V4.66683"
+      stroke="#9CA3AF"
       strokeWidth="1.5"
       strokeLinecap="round"
       strokeLinejoin="round"
