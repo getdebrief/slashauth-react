4826ae26a0d1bdcf2ec5a690fa4f892494a9a307
steve <steve21johnson@gmail.com>
2022-09-27 11:27:13 -0700

    top style, chevron

--- src/core/ui/components/drop-in/drop-down.tsx
+++ src/core/ui/components/drop-in/drop-down.tsx
@@ -57,6 +57,14 @@ export const DropDown = () => {
           {profilePicturePlaceholder}
         </div>
         {hashDisplay}
+        <div
+          style={{
+            marginLeft: 14,
+            marginRight: 13,
+          }}
+        >
+          {chevronDown}
+        </div>
       </div>
       {user.loggedIn && isOpen && (
         <Content>
@@ -245,6 +253,24 @@ const profilePicturePlaceholder = (
     />
   </svg>
 );
+const chevronDown = (
+  <svg
+    width="14"
+    height="9"
+    viewBox="0 0 14 9"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M12.8327 1.5L6.99935 7.33333L1.16602 1.5"
+      stroke="#9CA3AF"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
+
 const googleIcon = (
   <svg
     width="15"
