a3cccb8e11e932e9c57c79244ffe069bd630680c
steve <steve21johnson@gmail.com>
2022-09-27 11:23:13 -0700

    avatar is 36x36

--- src/core/ui/components/drop-in/drop-down.tsx
+++ src/core/ui/components/drop-in/drop-down.tsx
@@ -210,8 +210,8 @@ const Section = ({ children }) => (
 );
 const profilePicturePlaceholder = (
   <svg
-    width="42"
-    height="42"
+    width="36"
+    height="36"
     viewBox="0 0 42 42"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
