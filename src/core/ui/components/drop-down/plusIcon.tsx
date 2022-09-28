2bf882258df4cb862d034d3edb2c43ea947f1cbe
steve <steve21johnson@gmail.com>
2022-09-28 13:41:18 -0700

    svgs, own files

---
+++ src/core/ui/components/drop-down/plusIcon.tsx
@@ -0,0 +1,17 @@
+export const plusIcon = (
+  <svg
+    width="16"
+    height="16"
+    viewBox="0 0 16 16"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M7.99967 1.3335V14.6668M14.6663 8.00016L1.33301 8.00016"
+      stroke="#2F5FFC"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
