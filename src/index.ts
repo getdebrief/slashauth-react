5dbcb5f71b16b6ed45e3672691fb7c1b665f8572
steve <steve21johnson@gmail.com>
2022-10-05 09:12:23 -0700

    export TestProvider

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,16 @@ import {
   useSlashAuth,
 } from './core/context/legacy-slashauth';
 import { supportedFontFamilies } from './core/ui/fonts';
+import { TestProvider } from './core/ui/context/test';
 
 SlashAuth.mountComponentManager = mountComponentManager;
 
 export { useNetwork, useAccount } from './core/hooks';
 
-export { SlashAuth, SlashAuthProvider, useSlashAuth, supportedFontFamilies };
+export {
+  SlashAuth,
+  SlashAuthProvider,
+  useSlashAuth,
+  supportedFontFamilies,
+  TestProvider,
+};
