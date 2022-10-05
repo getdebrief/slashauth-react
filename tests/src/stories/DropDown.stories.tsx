69de29b1b842fe6bac6cbc88f166b24aa64f6fde
steve <steve21johnson@gmail.com>
2022-10-05 12:59:21 -0700

    LoggedIn story

--- tests/src/stories/DropDown.stories.tsx
+++ tests/src/stories/DropDown.stories.tsx
@@ -2,30 +2,10 @@ import React, { useEffect, useRef } from 'react';
 import { useSlashAuth } from '@slashauth/slashauth-react';
 import { userEvent, within } from '@storybook/testing-library';
 import { expect } from '@storybook/jest';
+import { ComponentStory } from '@storybook/react';
 
 export default {
   title: 'DropDown',
-};
-
-export const Primary = () => {
-  const context = useSlashAuth();
-  const { mountDropDown } = context;
-  const ref = useRef(null);
-  useEffect(() => {
-    if (ref.current) {
-      mountDropDown(ref.current, { loggedIn: true } as any);
-    }
-  }, [mountDropDown]);
-  return <div ref={ref} />;
-};
-Primary.play = async ({ canvasElement }: { canvasElement: HTMLElement }) => {
-  const canvas = within(canvasElement);
-  await canvas.findByTestId('DropDown');
-  const badge = canvas.getByTestId('DropDownBadge');
-  await userEvent.click(badge);
-  expect(canvas.getByText('Login to continue')).toBeInTheDocument();
-  await userEvent.click(badge);
-  expect(canvas.queryByText('Login to continue')).toBeNull();
 };
 type TestUser = {
   name?: string;
@@ -69,3 +49,29 @@ const testUser: { [k: string]: TestUser } = {
   },
 };
 const testCompany = 'Acme corp';
+
+export const Template: ComponentStory<any> = (args: { user: TestUser }) => {
+  const context = useSlashAuth();
+  const { mountDropDown } = context;
+  const ref = useRef(null);
+  useEffect(() => {
+    if (ref.current) {
+      mountDropDown(ref.current, args.user as any);
+    }
+  }, [args.user, mountDropDown]);
+  return <div ref={ref} />;
+};
+export const Primary = Template.bind({});
+Primary.play = async ({ canvasElement }: { canvasElement: HTMLElement }) => {
+  const canvas = within(canvasElement);
+  await canvas.findByTestId('DropDown');
+  const badge = canvas.getByTestId('DropDownBadge');
+  await userEvent.click(badge);
+  expect(canvas.getByText('Login to continue')).toBeInTheDocument();
+  await userEvent.click(badge);
+  expect(canvas.queryByText('Login to continue')).toBeNull();
+};
+export const LoggedIn = Template.bind({});
+LoggedIn.args = {
+  user: testUser.walletOnly,
+};
