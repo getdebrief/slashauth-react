b2c6582d0c890a5f32f837d8bce68f6b9a4522c1
Ned Rockson <nedrocks@gmail.com>
2022-11-04 17:30:38 -0700

    Updating hooks to return status rather than a function.

--- src/core/hooks/useHasOrgRole.tsx
+++ src/core/hooks/useHasOrgRole.tsx
@@ -1,12 +1,87 @@
+import { useState, useCallback, useEffect } from 'react';
 import { useCoreClient } from '../ui/context/slashauth-client';
 
-type hasOrgRole = (
-  organizationID: string,
-  roleName: string
-) => Promise<boolean>;
+type HasOrgRoleArgs = {
+  organizationID: string;
+  role: string;
+};
 
-export function useHasOrgRole(): hasOrgRole {
+type HookState = {
+  organizationID: string;
+  role: string;
+  loading: boolean;
+  hasOrgRole: boolean;
+  error: Error | null;
+};
+
+export function useHasOrgRole({ organizationID, role }: HasOrgRoleArgs) {
   const slashAuthClient = useCoreClient();
+  const [hookState, setHookState] = useState<HookState>({
+    organizationID,
+    role,
+    loading: false,
+    hasOrgRole: false,
+    error: null,
+  });
 
-  return slashAuthClient.hasOrgRole;
+  const fetchHasOrgRole = useCallback(async () => {
+    setHookState((prevState) => {
+      if (!prevState.loading) {
+        return { ...prevState, loading: true };
+      }
+      return prevState;
+    });
+    slashAuthClient
+      .hasOrgRole(organizationID, role)
+      .then((resp) => {
+        setHookState({
+          organizationID,
+          role,
+          loading: false,
+          hasOrgRole: resp,
+          error: null,
+        });
+      })
+      .catch((err) => {
+        setHookState({
+          organizationID,
+          role,
+          loading: false,
+          hasOrgRole: false,
+          error: err,
+        });
+      });
+  }, [organizationID, role, slashAuthClient]);
+
+  useEffect(() => {
+    if (
+      hookState.loading ||
+      (hookState.role === role && hookState.organizationID === organizationID)
+    ) {
+      return;
+    }
+    setHookState({
+      organizationID,
+      role,
+      loading: !!role && !!organizationID,
+      hasOrgRole: false,
+      error: null,
+    });
+    if (role && organizationID) {
+      fetchHasOrgRole();
+    }
+  }, [
+    fetchHasOrgRole,
+    hookState.loading,
+    hookState.organizationID,
+    hookState.role,
+    organizationID,
+    role,
+    slashAuthClient,
+  ]);
+
+  return {
+    ...hookState,
+    refetch: fetchHasOrgRole,
+  };
 }
--- src/core/hooks/useHasRole.tsx
+++ src/core/hooks/useHasRole.tsx
@@ -1,9 +1,66 @@
+import { useCallback, useEffect, useState } from 'react';
 import { useCoreClient } from '../ui/context/slashauth-client';
 
-type hasRole = (role: string) => Promise<boolean>;
+type HookState = {
+  role: string;
+  loading: boolean;
+  hasRole: boolean;
+  error: Error | null;
+};
 
-export function useHasRole(): hasRole {
+export function useHasRole(role: string) {
   const slashAuthClient = useCoreClient();
+  const [hookState, setHookState] = useState<HookState>({
+    role,
+    loading: false,
+    hasRole: false,
+    error: null,
+  });
 
-  return slashAuthClient.hasRole;
+  const fetchHasRole = useCallback(async () => {
+    setHookState((prevState) => {
+      if (!prevState.loading) {
+        return { ...prevState, loading: true };
+      }
+      return prevState;
+    });
+    slashAuthClient
+      .hasRole(role)
+      .then((resp) => {
+        setHookState({
+          role,
+          loading: false,
+          hasRole: resp,
+          error: null,
+        });
+      })
+      .catch((err) => {
+        setHookState({
+          role,
+          loading: false,
+          hasRole: false,
+          error: err,
+        });
+      });
+  }, [role, slashAuthClient]);
+
+  useEffect(() => {
+    if (hookState.loading || hookState.role === role) {
+      return;
+    }
+    setHookState({
+      role,
+      loading: !!role,
+      hasRole: false,
+      error: null,
+    });
+    if (role) {
+      fetchHasRole();
+    }
+  }, [fetchHasRole, hookState.loading, hookState.role, role, slashAuthClient]);
+
+  return {
+    ...hookState,
+    refetch: fetchHasRole,
+  };
 }
