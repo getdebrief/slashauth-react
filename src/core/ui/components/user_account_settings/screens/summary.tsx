8898c0fe7b16334798a12662d4c3e2b4283dbd9c
donbernar <alvaro@slashauth.com>
2023-01-27 10:54:16 -0600

    Finish summary page

--- src/core/ui/components/user_account_settings/screens/summary.tsx
+++ src/core/ui/components/user_account_settings/screens/summary.tsx
@@ -5,8 +5,70 @@ import { Text, Align } from '../../primitives/text';
 import { Header } from '../layout/header';
 import { useRouter } from '../../../router/context';
 import { useMemo } from 'react';
+import { Icon } from '../../drop-down/icon';
+import { EthLogo } from '../../icon/ethereum-eth-logo';
+import { Chip } from '../../primitives/chip';
+import { Avatar } from '../../primitives/avatar';
+import { MailIcon } from '@heroicons/react/outline';
 
-export const SummaryScreen = ({ accountSettings }) => {
+const ConnectionTypeIcon = ({ type }) => {
+  const style = {
+    height: '20px',
+    width: '20px',
+    display: 'flex',
+    marginRight: '8px',
+  };
+
+  switch (type) {
+    case 'google':
+      return (
+        <img
+          style={style}
+          src="https://d1l2xccggl7xwv.cloudfront.net/icons/google.png"
+          alt="Google logo"
+        />
+      );
+    case 'magic_link':
+      return (
+        <MailIcon
+          style={{ width: 18, height: 18, color: '#B6BCC8', ...style }}
+        />
+      );
+    case 'managed_wallet':
+    case 'eth_wallet_signature':
+      return <Icon style={style}>{EthLogo}</Icon>;
+    default:
+      return null;
+  }
+};
+
+const BinIcon = ({ onClick }) => (
+  <span className={styles.deleteAction} onClick={onClick}>
+    <svg
+      width="16"
+      height="18"
+      viewBox="0 0 16 18"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M13.833 4.83333L13.1102 14.9521C13.0479 15.8243 12.3222 16.5 11.4478 16.5H4.55154C3.67714 16.5 2.95141 15.8243 2.88911 14.9521L2.16634 4.83333M6.33301 8.16667V13.1667M9.66634 8.16667V13.1667M10.4997 4.83333V2.33333C10.4997 1.8731 10.1266 1.5 9.66634 1.5H6.33301C5.87277 1.5 5.49967 1.8731 5.49967 2.33333V4.83333M1.33301 4.83333H14.6663"
+        stroke="#4B5563"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  </span>
+);
+
+export const SummaryScreen = ({
+  accountSettings,
+  removeConnection,
+  editProfilePicture,
+  addEmail,
+  addWallet,
+}) => {
   const router = useRouter();
 
   if (accountSettings === null) {
@@ -29,6 +91,14 @@ export const SummaryScreen = ({ accountSettings }) => {
     };
   }, [accountSettings?.connections]);
 
+  const nonCustodialWallets = useMemo(
+    () =>
+      web3Wallets.filter(
+        (connection) => connection.connectionType !== 'managed_wallet'
+      ),
+    [web3Wallets]
+  );
+
   return accountSettings ? (
     <>
       <Header
@@ -38,55 +108,122 @@ export const SummaryScreen = ({ accountSettings }) => {
       <Text component="h2" align={Align.Left} className={margin.top8}>
         Profile Image
       </Text>
-      <div className={classNames(styles.profileImage, margin.top2)}>
-        <img
-          src="https://avatars.githubusercontent.com/u/3150991?s=60&v=4"
-          alt=""
-        />
-      </div>
-      <Text component="h2" align={Align.Left} className={margin.top12}>
+      <Avatar
+        className={margin.top2}
+        src={accountSettings.defaultProfileImage}
+        onClick={editProfilePicture}
+      />
+      <Text
+        component="h2"
+        align={Align.Left}
+        className={classNames(margin.top12, margin.bottom2)}
+      >
         Name
       </Text>
-      <div>{accountSettings.name}</div>
+      <div className={styles.action} onClick={() => router.navigate('name')}>
+        {accountSettings.name ? (
+          <Text align={Align.Left} className={classNames(styles.settingsValue)}>
+            {accountSettings.name}
+          </Text>
+        ) : (
+          <Text className={classNames(styles.addButton)} align={Align.Left}>
+            + Add name
+          </Text>
+        )}
+        <span className={styles.navigateAction}>
+          <svg
+            width="18"
+            height="14"
+            viewBox="0 0 18 14"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M10.6667 1.16667L16.5 7.00001M16.5 7.00001L10.6667 12.8333M16.5 7.00001L1.5 7"
+              stroke="#2F5FFC"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+          </svg>
+        </span>
+      </div>
       <Text
-        component="a"
-        className={classNames(styles.addButton, margin.top2)}
+        component="h2"
         align={Align.Left}
-        onClick={() => router.navigate('./name')}
+        className={classNames(margin.top12, margin.bottom2)}
       >
-        + Add name
-      </Text>
-      <Text component="h2" align={Align.Left} className={margin.top12}>
         Email address
       </Text>
-      {emails.map((connection) => (
-        <Text
-          align={Align.Left}
-          className={classNames(styles.settingsValue, margin.top2)}
-        >
-          {connection.connectionType} - {connection.displayValue}
-        </Text>
-      ))}
-      <Text component="h2" align={Align.Left} className={margin.top12}>
+      {emails.length ? (
+        emails.map((connection) => (
+          <div className={styles.actionOnHover} key={connection.id}>
+            <div style={{ display: 'flex', alignItems: 'center' }}>
+              <ConnectionTypeIcon type={connection.connectionType} />
+              <Text
+                align={Align.Left}
+                className={classNames(styles.settingsValue)}
+              >
+                {connection.displayValue}
+              </Text>
+              <Chip>Verified</Chip>
+            </div>
+            <BinIcon onClick={() => removeConnection(connection.id)} />
+          </div>
+        ))
+      ) : (
+        <div className={styles.action} onClick={addEmail}>
+          <Text className={classNames(styles.addButton)} align={Align.Left}>
+            + Add email
+          </Text>
+        </div>
+      )}
+      <Text
+        component="h2"
+        align={Align.Left}
+        className={classNames(margin.top12, margin.bottom2)}
+      >
         Web3 wallets
       </Text>
-      {web3Wallets.map((connection) => (
-        <Text
-          align={Align.Left}
-          className={classNames(styles.settingsValue, margin.top2)}
-        >
-          {connection.connectionType} - {connection.displayValue}
-        </Text>
-      ))}
-      <Text component="h2" align={Align.Left} className={margin.top12}>
+      {web3Wallets.length
+        ? web3Wallets.map((connection) => (
+            <div className={styles.actionOnHover} key={connection.id}>
+              <div style={{ display: 'flex', alignItems: 'center' }}>
+                <ConnectionTypeIcon type={connection.connectionType} />
+                <Text
+                  align={Align.Left}
+                  className={classNames(styles.settingsValue)}
+                >
+                  {connection.displayValue}
+                </Text>
+              </div>
+              <BinIcon onClick={() => removeConnection(connection.id)} />
+            </div>
+          ))
+        : null}
+      {!nonCustodialWallets.length ? (
+        <div className={styles.action} onClick={addWallet}>
+          <Text className={classNames(styles.addButton)} align={Align.Left}>
+            + Add wallet
+          </Text>
+        </div>
+      ) : null}
+      {/* <Text
+        component="h2"
+        align={Align.Left}
+        className={classNames(margin.top12, margin.bottom2)}
+      >
         Web2 accounts
       </Text>
-      <Text
-        className={classNames(styles.addButton, margin.top2)}
-        align={Align.Left}
-      >
-        + Add social account
-      </Text>
+      <div className={styles.action}>
+        <Text
+          className={classNames(styles.addButton)}
+          align={Align.Left}
+          onClick={() => onAddConnection(LoginMethodType.MagicLink)}
+        >
+          + Add social account
+        </Text>
+      </div> */}
     </>
   ) : // loading screen
   null;
