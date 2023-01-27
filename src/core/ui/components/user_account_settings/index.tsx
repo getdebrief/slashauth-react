a1aa7a9ba8413ad6fd12bf5d26c3df6375885ef2
donbernar <alvaro@slashauth.com>
2023-01-27 10:57:08 -0600

    Put everything together in the UserAccountSettings component

--- src/core/ui/components/user_account_settings/index.tsx
+++ src/core/ui/components/user_account_settings/index.tsx
@@ -4,22 +4,54 @@ import { EditNameScreen } from './screens/edit_name';
 import { SummaryScreen } from './screens/summary';
 import { Wrapper } from './layout/wrapper';
 import { VirtualRouter } from '../../router/virtual';
-import { useFetchMyAccountSettings } from './useFetchMyAccountSettings';
+import { useSlashauthClientUserAccountSettings } from './useSlashauthClientUserAccountSettings';
+import { useCallback } from 'react';
+import { LoginMethodType } from '../../context/login-methods';
 
 export const UserAccountSettings = withVirtualRouter(() => {
-  const myAccountSettings = useFetchMyAccountSettings();
+  const {
+    accountSettings,
+    patchAccountSettings,
+    removeConnection,
+    addConnection,
+  } = useSlashauthClientUserAccountSettings();
   const router = useRouter();
+
+  const addEmail = useCallback(() => {
+    addConnection([LoginMethodType.FederatedGoogle, LoginMethodType.MagicLink]);
+  }, [addConnection]);
+
+  const addWallet = useCallback(() => {
+    addConnection([LoginMethodType.Web3]);
+  }, [addConnection]);
+
+  // const addWeb2Account = useCallback(() => {
+  //   openSignIn({
+  //     connectAccounts: true,
+  //     includeLoginMethodTypes: [
+  //       LoginMethodType.FederatedDiscord,
+  //       LoginMethodType.FederatedTwitter,
+  //     ],
+  //   });
+  //   return;
+  // }, [openSignIn]);
 
   return (
     <Wrapper>
       <Route path="name">
         <EditNameScreen
           backToSettings={() => router.navigate('')}
-          save={console.log}
+          save={(name) => patchAccountSettings({ name })}
         />
       </Route>
       <Route index>
-        <SummaryScreen accountSettings={myAccountSettings} />
+        <SummaryScreen
+          accountSettings={accountSettings}
+          removeConnection={removeConnection}
+          editProfilePicture={console.log}
+          addEmail={addEmail}
+          addWallet={addWallet}
+        />
       </Route>
     </Wrapper>
   );
